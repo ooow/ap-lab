c080e9b7d96effac138df504218a5ccfa3815d39
vnazarchuk <vnazarchuk@griddynamics.com>
2021-04-08 15:11:00 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tk-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-time-killer';
+}
