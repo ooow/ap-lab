b69baaf737e2ccfd05ef78554b0794110a7b4154
skluba <skluba@griddynamics.com>
2022-02-04 20:23:15 +0100

    add: cutom directive for conditional display

---
+++ src/app/shared/directives/if-view-mode.directive.ts
@@ -0,0 +1,42 @@
+import { Directive, Input, OnDestroy, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { ProductsView } from '../models/products-view';
+import * as ProductsViewSelectors from '../store/products-view/products-view.selectors';
+
+@Directive({
+  selector: '[tkIfViewMode]'
+})
+export class IfViewModeDirective implements OnDestroy {
+
+  private hasView = false;
+  private readonly destroy$ = new Subject();
+
+  constructor(
+    private store: Store,
+    private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef,
+  ) { }
+
+  @Input() set tkIfViewMode(mode: ProductsView) {
+    this.store.select(ProductsViewSelectors.productsViewSelector)
+    .pipe(
+      takeUntil(this.destroy$)
+    )
+    .subscribe(viewMode => {
+      if (mode === viewMode && !this.hasView) {
+        console.log("false")
+        this.viewContainer.createEmbeddedView(this.templateRef);
+        this.hasView = true;
+      } else if (mode !== viewMode && this.hasView) {
+        this.viewContainer.clear();
+        this.hasView = false;
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+  }
+}
