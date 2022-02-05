f4e0f31b29dd2249e867c1f8354232af5ef1667e
skluba <skluba@griddynamics.com>
2022-02-05 22:42:45 +0100

    add: tests

--- src/app/products/products.component.spec.ts
+++ src/app/products/products.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { HarnessLoader } from '@angular/cdk/testing';
 import { Component, DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -8,6 +9,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { MockComponent, MockPipe, MockProvider } from 'ng-mocks';
 import { of } from 'rxjs';
+import { MatButtonToggleGroupHarness, MatButtonToggleHarness } from '@angular/material/button-toggle/testing';
 
 import { ProductDetailsModalComponent } from 'src/app/products/components/product-details-modal/product-details-modal.component';
 import { ProductTableComponent } from 'src/app/products/components/product-table/product-table.component';
@@ -22,6 +24,11 @@ import { getProductsAction } from 'src/app/shared/store/product/actions/get-prod
 import { deleteProductAction } from 'src/app/shared/store/stored-product/actions/delete-product.actions';
 import { getTopProductsAction } from 'src/app/shared/store/top-products/actions/get-top-products.action';
 import { isLoading } from 'src/app/shared/store/top-products/top-products.selectors';
+import { IfViewModeDirective } from '../shared/directives/if-view-mode.directive';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { viewModes } from './mocks/view-modes';
+import { changeViewAction } from '../shared/store/products-view/products-view.actions';
+
 
 describe('Products', () => {
   @Component({
@@ -40,14 +47,15 @@ describe('Products', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NoopAnimationsModule, MatDialogModule, RouterTestingModule],
+      imports: [NoopAnimationsModule, MatDialogModule, RouterTestingModule, MatButtonToggleModule],
       declarations: [
         TestComponent,
         ProductsComponent,
         MockPipe(ProductSearchPipe, (products) => products),
         MockComponent(ProductComponent),
         MockComponent(ProductTableComponent),
-        MockComponent(ProductDetailsModalComponent)
+        MockComponent(ProductDetailsModalComponent),
+        IfViewModeDirective
       ],
       providers: [
         provideMockStore({ initialState }),
@@ -98,10 +106,54 @@ describe('Products', () => {
     );
   });
 
+  describe('View mode selector', () => {
+    let loader: HarnessLoader;
+    let group: MatButtonToggleGroupHarness;
+
+    beforeEach(async () => {
+      loader = TestbedHarnessEnvironment.loader(fixture);
+      group = await loader.getHarness(MatButtonToggleGroupHarness)
+    })
+    
+    it('should render correct number of groups', async () => {
+      const groups = await loader.getAllHarnesses(MatButtonToggleGroupHarness);
+      expect(groups.length).toBe(1);
+    });
+
+    describe('Toggles', () => {
+      let toggles: MatButtonToggleHarness[];
+      
+      beforeEach(async () => {
+        toggles = await group.getToggles();
+      })
+
+      it('should render correct number of toggles', () => {
+        expect(toggles.length).toBe(viewModes.length)
+      });
+  
+      it('should render toggles with correct props', async () => {
+        expect(await toggles[0].getText()).toBe(viewModes[0].icon);
+        expect(await toggles[0].getAriaLabel()).toBe(viewModes[0].label);
+      });
+
+      it('should listen to view mode changes', async () => {
+        spyOn(store, "dispatch").and.callFake(() => {})
+        expect(store.dispatch).not.toHaveBeenCalled();
+        await toggles[1].toggle();
+        expect(store.dispatch).toHaveBeenCalledOnceWith(
+          changeViewAction({ mode: viewModes[1].mode })
+        )
+      });
+    });
+  });
+
   describe('Product Components', () => {
     let productCards: DebugElement[];
 
     beforeEach(() => {
+      store.setState({...initialState, productsView: 'cards'});
+      fixture.detectChanges();
+
       productCards = fixture.debugElement.queryAll(
         By.directive(ProductComponent)
       );
@@ -125,53 +177,53 @@ describe('Products', () => {
       const expectedArgs = initialState.product.products[0];
       expect(spy).toHaveBeenCalledOnceWith(expectedArgs);
     });
+  });
 
-    describe('Product Table', () => {
-      let productTable: ProductTableComponent;
+  describe('Product Table', () => {
+    let productTable: ProductTableComponent;
 
-      beforeEach(() => {
-        productTable = fixture.debugElement.query(
-          By.directive(ProductTableComponent)
-        ).componentInstance;
-      });
+    beforeEach(() => {
+      productTable = fixture.debugElement.query(
+        By.directive(ProductTableComponent)
+      ).componentInstance;
+    });
 
-      it('should render product table', () => {
-        expect(productTable).toBeTruthy();
-      });
+    it('should render product table', () => {
+      expect(productTable).toBeTruthy();
+    });
 
-      it('should pass correct props to product table', () => {
-        const {
-          products,
-          pageIndex,
-          search,
-          totalNumber
-        } = initialState.product;
-        expect(productTable.loading).toEqual(false);
-        expect(productTable.products).toEqual(products);
-        expect(productTable.pageIndex).toEqual(pageIndex);
-        expect(productTable.search).toEqual(search);
-        expect(productTable.totalNumber).toEqual(totalNumber);
-      });
+    it('should pass correct props to product table', () => {
+      const {
+        products,
+        pageIndex,
+        search,
+        totalNumber
+      } = initialState.product;
+      expect(productTable.loading).toEqual(false);
+      expect(productTable.products).toEqual(products);
+      expect(productTable.pageIndex).toEqual(pageIndex);
+      expect(productTable.search).toEqual(search);
+      expect(productTable.totalNumber).toEqual(totalNumber);
+    });
 
-      it('should listen to page changes', () => {
-        spyOn(store, 'dispatch').and.callFake(() => {});
-        expect(store.dispatch).toHaveBeenCalledTimes(0);
-        const pageIndex = 666;
-        productTable.pageChange.emit(pageIndex);
-        expect(store.dispatch).toHaveBeenCalledOnceWith(
-          changePageAction({ pageIndex })
-        );
-      });
+    it('should listen to page changes', () => {
+      spyOn(store, 'dispatch').and.callFake(() => {});
+      expect(store.dispatch).toHaveBeenCalledTimes(0);
+      const pageIndex = 666;
+      productTable.pageChange.emit(pageIndex);
+      expect(store.dispatch).toHaveBeenCalledOnceWith(
+        changePageAction({ pageIndex })
+      );
+    });
 
-      it('should listen to delete product events', () => {
-        spyOn(store, 'dispatch').and.callFake(() => {});
-        expect(store.dispatch).toHaveBeenCalledTimes(0);
-        const productItem = initialState.product[0];
-        productTable.deleteProduct.emit(productItem);
-        expect(store.dispatch).toHaveBeenCalledOnceWith(
-          deleteProductAction({ lang: initialState.lang, product: productItem })
-        );
-      });
+    it('should listen to delete product events', () => {
+      spyOn(store, 'dispatch').and.callFake(() => {});
+      expect(store.dispatch).toHaveBeenCalledTimes(0);
+      const productItem = initialState.product[0];
+      productTable.deleteProduct.emit(productItem);
+      expect(store.dispatch).toHaveBeenCalledOnceWith(
+        deleteProductAction({ lang: initialState.lang, product: productItem })
+      );
     });
   });
 });
