225fd2a936fa4521f580becaf58aad0a5491262e
Vamsi Sai Krishna <vamsisaikrishna.y@gmail.com>
2023-03-07 15:28:17 +0530

    PR changes fixed

--- src/app/products/components/product-delete-confirm-dialog/product-delete-confirm-dialog.component.spec.ts
+++ src/app/products/components/product-delete-confirm-dialog/product-delete-confirm-dialog.component.spec.ts
@@ -1,31 +1,31 @@
-import { HarnessLoader } from "@angular/cdk/testing";
-import { TestbedHarnessEnvironment } from "@angular/cdk/testing/testbed";
-import { Component } from "@angular/core";
-import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { MatButtonModule } from "@angular/material/button";
-import { MatDialog, MatDialogModule } from "@angular/material/dialog";
-import { MatDialogHarness } from "@angular/material/dialog/testing";
-import { NoopAnimationsModule } from "@angular/platform-browser/animations";
-import { take } from "rxjs/operators";
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatDialogHarness } from '@angular/material/dialog/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { take } from 'rxjs/operators';
 
-import { Product } from "src/app/shared/models/product";
-import { ProductDeleteConfirmDialogComponent } from "./product-delete-confirm-dialog.component";
-import { ProductDeleteConfirmDialogHarness } from "./product-delete-confirm-dialog.harness";
+import { Product } from 'src/app/shared/models/product';
+import { ProductDeleteConfirmDialogComponent } from './product-delete-confirm-dialog.component';
+import { ProductDeleteConfirmDialogHarness } from './product-delete-confirm-dialog.harness';
 
 const product = {
-  name: "test product",
-  description: "test description",
-  picture: "picture url"
-}
+  name: 'test product',
+  description: 'test description',
+  picture: 'picture url'
+};
 
-describe("AppModule => ProductDeleteConfirmDialogComponent", () => {
+describe('ProductDeleteConfirmDialogComponent', () => {
   let fixture: ComponentFixture<TestComponent>;
   let component: TestComponent;
   let dialogHarness: ProductDeleteConfirmDialogHarness;
   let rootLoader: HarnessLoader;
 
   @Component({
-    selector: "tk-test",
+    selector: 'tk-test',
     template: "<button (click)='openDialog()'></button>"
   })
   class TestComponent {
@@ -34,27 +34,22 @@ describe("AppModule => ProductDeleteConfirmDialogComponent", () => {
     constructor(public dialog: MatDialog) {}
 
     openDialog(): void {
-      const dialogRef = this.dialog.open(
-        ProductDeleteConfirmDialogComponent,
-        { data: product },
-      )
+      const dialogRef = this.dialog.open(ProductDeleteConfirmDialogComponent, {
+        data: product
+      });
 
       dialogRef
         .afterClosed()
         .pipe(take(1))
         .subscribe((value) => {
-            this.receivedData = value;
+          this.receivedData = value;
         });
-      }
     }
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        MatDialogModule,
-        MatButtonModule,
-        NoopAnimationsModule
-      ],
+      imports: [MatDialogModule, MatButtonModule, NoopAnimationsModule],
       declarations: [ProductDeleteConfirmDialogComponent, TestComponent]
     });
 
@@ -66,45 +61,47 @@ describe("AppModule => ProductDeleteConfirmDialogComponent", () => {
     fixture.detectChanges();
     await fixture.whenStable();
 
-    dialogHarness = await rootLoader.getHarness(ProductDeleteConfirmDialogHarness);
-  })
-
-  it("should be created", () => {
-    expect(component).not.toBeNull();
+    dialogHarness = await rootLoader.getHarness(
+      ProductDeleteConfirmDialogHarness
+    );
   });
 
-  it("should be rendered appropriately", async () => {
-    expect(await (await dialogHarness.getTitle()).text()).toBe("Please confirm");
-    expect(await (await dialogHarness.getBody()).text()).toContain(product.name);
+  it('renders component', async () => {
+    expect(await (await dialogHarness.getTitle()).text()).toBe(
+      'Please confirm'
+    );
+    expect(await (await dialogHarness.getBody()).text()).toContain(
+      product.name
+    );
     expect(await dialogHarness.getCancelButton()).not.toBeNull();
     expect(await dialogHarness.getDeleteButton()).not.toBeNull();
-  })
-
-  describe("cancel button", () => {
-      it("should not emit data on click", async () => {
-        const cancelButton = await dialogHarness.getCancelButton();
-        await cancelButton.click();
-        expect(component.receivedData).toBeUndefined();
-      });
-
-      it("should close dialog", async () => {
-        const cancelButton = await dialogHarness.getCancelButton();
-        await cancelButton.click();
-        expect(await rootLoader.getAllHarnesses(MatDialogHarness)).toHaveSize(0);
-      });
   });
 
-  describe("delete button", () => {
-      it("should emit data on click", async () => {
-        const deleteButton = await dialogHarness.getDeleteButton();
-        await deleteButton.click();
-        expect(component.receivedData).toEqual(product)
-      });
+  describe('cancel button', () => {
+    it('should not emit data on click', async () => {
+      const cancelButton = await dialogHarness.getCancelButton();
+      await cancelButton.click();
+      expect(component.receivedData).toBeUndefined();
+    });
 
-      it("should close dialog", async () => {
-        const deleteButton = await dialogHarness.getDeleteButton();
-        await deleteButton.click();
-        expect(await rootLoader.getAllHarnesses(MatDialogHarness)).toHaveSize(0);
-      })
-  })
+    it('should close dialog', async () => {
+      const cancelButton = await dialogHarness.getCancelButton();
+      await cancelButton.click();
+      expect(await rootLoader.getAllHarnesses(MatDialogHarness)).toHaveSize(0);
+    });
+  });
+
+  describe('delete button', () => {
+    it('should emit data on click', async () => {
+      const deleteButton = await dialogHarness.getDeleteButton();
+      await deleteButton.click();
+      expect(component.receivedData).toEqual(product);
+    });
+
+    it('should close dialog', async () => {
+      const deleteButton = await dialogHarness.getDeleteButton();
+      await deleteButton.click();
+      expect(await rootLoader.getAllHarnesses(MatDialogHarness)).toHaveSize(0);
+    });
+  });
 });
--- src/app/products/components/product-delete-confirm-dialog/product-delete-confirm-dialog.component.ts
+++ src/app/products/components/product-delete-confirm-dialog/product-delete-confirm-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ProductDeleteConfirmation } from 'src/app/shared/models/product-delete-confirmation';
+
 @Component({
   selector: 'tk-product-delete-confirm-dialog',
   templateUrl: './product-delete-confirm-dialog.component.html',
