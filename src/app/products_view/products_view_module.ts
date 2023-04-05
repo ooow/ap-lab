3f8723bce61b374169ac4473bd6632e5e08733e3
Varsha <varshamathukumalli@gmail.com>
2023-04-05 19:22:27 +0530

    Removed formatter changes

--- src/app/products_view/products_view_module.ts
+++ src/app/products_view/products_view_module.ts
@@ -1,35 +1,34 @@
-import { ClipboardModule } from '@angular/cdk/clipboard';
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatIconModule } from '@angular/material/icon';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { RouterModule } from '@angular/router';
-import { A11yModule } from '@angular/cdk/a11y';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import {ClipboardModule} from '@angular/cdk/clipboard';
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatIconModule} from '@angular/material/icon';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {MatSortModule} from '@angular/material/sort';
+import {MatTableModule} from '@angular/material/table';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {RouterModule} from '@angular/router';
+import {A11yModule} from '@angular/cdk/a11y';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 
-import { LoaderModule } from 'src/app/shared/component/loader/loader.module';
-import { ProductDetailsDialog } from './product_details_dialog/product_details_dialog';
-import { ProductTableComponent } from './product_table/product_table';
-import { ProductCard } from './product_card/product_card';
-import { ProductSearchPipe } from './pipe/product_search_pipe';
-import { ProductsView } from './products_view';
-import { ProductDeleteDialog } from './product_delete_dialog/product_delete_dialog';
-import { CreateProductDialog } from './create_product_dialog/create_product_dialog';
-import { ProductAvailabilityDetails } from './product_availability_details/product_availability_details';
+import {LoaderModule} from 'src/app/shared/component/loader/loader.module';
+import {ProductDetailsDialog} from './product_details_dialog/product_details_dialog';
+import {ProductTableComponent} from './product_table/product_table';
+import {ProductCard} from './product_card/product_card';
+import {ProductSearchPipe} from './pipe/product_search_pipe';
+import {ProductsView} from './products_view';
+import {ProductDeleteDialog} from './product_delete_dialog/product_delete_dialog';
+import {CreateProductDialog} from './create_product_dialog/create_product_dialog';
 
-const routes = [{ path: 'products', component: ProductsView }];
+const routes = [{path: 'products', component: ProductsView}];
 
 @NgModule({
   declarations: [
@@ -40,9 +39,7 @@ const routes = [{ path: 'products', component: ProductsView }];
     ProductSearchPipe,
     ProductsView,
     ProductTableComponent,
-    ProductAvailabilityDetails,
-  ],
-  imports: [
+  ], imports  : [
     A11yModule,
     ClipboardModule,
     CommonModule,
@@ -62,7 +59,6 @@ const routes = [{ path: 'products', component: ProductsView }];
     MatSelectModule,
     ReactiveFormsModule,
     RouterModule.forChild(routes),
-  ],
-  providers: [MatSnackBar],
+  ], providers: [MatSnackBar],
 })
 export class ProductsViewModule {}
--- src/app/shared/component/header/lang-selector/lang-selector.component.spec.ts
+++ src/app/shared/component/header/lang-selector/lang-selector.component.spec.ts
@@ -1,19 +1,16 @@
-import { HarnessLoader } from '@angular/cdk/testing';
-import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
-import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSelectHarness } from '@angular/material/select/testing';
-import { By } from '@angular/platform-browser';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {HarnessLoader} from '@angular/cdk/testing';
+import {TestbedHarnessEnvironment} from '@angular/cdk/testing/testbed';
+import {Component, NO_ERRORS_SCHEMA} from '@angular/core';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSelectHarness} from '@angular/material/select/testing';
+import {By} from '@angular/platform-browser';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
-import {
-  LangSelectorComponent,
-  Language,
-} from 'src/app/shared/component/header/lang-selector/lang-selector.component';
-import { LANGUAGES_TOKEN } from 'src/app/store/token/languages.token';
-// import {LangSelectorOptionComponent} from 'src/app/shared/component/header/components/lang-selector-option/lang-selector-option.component';
-import { LangSelectorHarness } from 'src/app/shared/component/header/lang-selector/lang-selector.harness';
+import {LangSelectorComponent, Language} from 'src/app/shared/component/header/lang-selector/lang-selector.component';
+import {LANGUAGES_TOKEN} from 'src/app/store/token/languages.token';
+import {LangSelectorOptionComponent} from 'src/app/shared/component/header/components/lang-selector-option/lang-selector-option.component';
+import {LangSelectorHarness} from 'src/app/shared/component/header/lang-selector/lang-selector.harness';
 
 @Component({
   selector: 'tk-test',
@@ -31,15 +28,13 @@ describe('AppModule => LangSelectorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MatSelectModule, NoopAnimationsModule],
-      declarations: [LangSelectorComponent, TestComponent],
-      providers: [
+      imports     : [MatSelectModule, NoopAnimationsModule], declarations: [
+        LangSelectorComponent, TestComponent, LangSelectorOptionComponent,
+      ], providers: [
         {
-          provide: LANGUAGES_TOKEN,
-          useValue: languages,
+          provide: LANGUAGES_TOKEN, useValue: languages,
         },
-      ],
-      schemas: [NO_ERRORS_SCHEMA],
+      ], schemas  : [NO_ERRORS_SCHEMA],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TestComponent);
@@ -60,37 +55,40 @@ describe('AppModule => LangSelectorComponent', () => {
   });
 
   // tslint:disable-next-line:max-line-length
-  it('should preset provided default language for selector component', async () => {
-    const harness = await loader.getHarness(MatSelectHarness);
+  it('should preset provided default language for selector component',
+    async () => {
+      const harness = await loader.getHarness(MatSelectHarness);
 
-    component.lang = Language.ru;
-    fixture.detectChanges();
-    await fixture.whenStable();
+      component.lang = Language.ru;
+      fixture.detectChanges();
+      await fixture.whenStable();
 
-    expect(await harness.getValueText()).toBe('Russian');
-  });
-
-  // tslint:disable-next-line:max-line-length
-  it('should preset provided default language for selector component', async () => {
-    const harness = await loader.getHarness(MatSelectHarness);
-
-    component.lang = Language.ru;
-    fixture.detectChanges();
-    await fixture.whenStable();
-
-    expect(await harness.getValueText()).toBe('Russian');
-  });
+      expect(await harness.getValueText()).toBe('Russian');
+    });
 
   // tslint:disable-next-line:max-line-length
-  it('should show selector options matching LANGUAGES_TOKEN provided value', async () => {
-    const selectorHarness = await loader.getHarness(MatSelectHarness);
-    await selectorHarness.open();
-    const options = await selectorHarness.getOptions();
+  it('should preset provided default language for selector component',
+    async () => {
+      const harness = await loader.getHarness(MatSelectHarness);
 
-    expect(options.length).toBe(languages.length);
-    expect(await options[0].getText()).toBe('Russian');
-    expect(await options[1].getText()).toBe('English');
-  });
+      component.lang = Language.ru;
+      fixture.detectChanges();
+      await fixture.whenStable();
+
+      expect(await harness.getValueText()).toBe('Russian');
+    });
+
+  // tslint:disable-next-line:max-line-length
+  it('should show selector options matching LANGUAGES_TOKEN provided value',
+    async () => {
+      const selectorHarness = await loader.getHarness(MatSelectHarness);
+      await selectorHarness.open();
+      const options = await selectorHarness.getOptions();
+
+      expect(options.length).toBe(languages.length);
+      expect(await options[0].getText()).toBe('Russian');
+      expect(await options[1].getText()).toBe('English');
+    });
 
   it('should propagate selected option language value', async () => {
     // prettier-ignore
